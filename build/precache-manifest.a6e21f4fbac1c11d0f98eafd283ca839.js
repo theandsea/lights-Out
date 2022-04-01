7728acaf5eed531a9d2fd868fde1cb80159c0061
theandsea <45749967+theandsea@users.noreply.github.com>
2022-04-01 01:41:53 -0700

    Add files via upload

---
+++ build/precache-manifest.a6e21f4fbac1c11d0f98eafd283ca839.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "130d0ec37301e66db54db151ab4f93e4",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5d5c8a1a482e3ee27455",
+    "url": "./static/css/main.890b698c.chunk.css"
+  },
+  {
+    "revision": "030616dcf8ea189fa3f7",
+    "url": "./static/js/2.2d2ab74d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.2d2ab74d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d5c8a1a482e3ee27455",
+    "url": "./static/js/main.824410c1.chunk.js"
+  },
+  {
+    "revision": "7304c6cd1204852fb3fb",
+    "url": "./static/js/runtime-main.1451e3c5.js"
+  }
+]);
